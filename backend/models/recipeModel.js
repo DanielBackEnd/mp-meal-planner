6d8d3fa2c6cfc3386f22be15d9dd4f0f565b0d01
DanielBackEnd <danielgithubb@gmail.com>
2024-10-16 12:29:09 +0200

    created recipe model

---
+++ backend/models/recipeModel.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+
+const recipeSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'User',
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  indegrients: {
+    type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Product' }],
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+});
