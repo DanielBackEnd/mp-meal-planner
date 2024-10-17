622ceba5865f5d571926c5959e93c1b52a370f0a
DanielBackEnd <danielgithubb@gmail.com>
2024-10-17 12:44:19 +0200

    created custom error middleware

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 const PORT = process.env.PORT;
 
 connectDB();
@@ -11,5 +12,8 @@ const app = express();
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
