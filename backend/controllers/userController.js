917a1901f2f86679f3be367de07634507c27c8ac
DanielBackEnd <danielgithubb@gmail.com>
2024-10-20 17:36:50 +0200

    created login functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,11 +1,36 @@
 import asyncHandler from '../middleware/asyncHandler.js';
 import User from '../models/userModel.js';
+import jwt from 'jsonwebtoken';
 
 // login & get token
 // GET /api/users/login
 // public
 const loginUser = asyncHandler(async (req, res) => {
-  res.send('login user');
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    const token = jwt.sign({ userId: user._id }, process.env.JWT.SECRET, {
+      expiresIn: '30d',
+    });
+
+    res.cookie('jwt', token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV !== 'development',
+      sameSite: 'strict',
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+    });
+
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(401);
+    throw new Error('Invalid email or password');
+  }
 });
 
 // register
