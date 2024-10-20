fb743e34e04a203eee1fc1ab8666b57a890c0945
DanielBackEnd <danielgithubb@gmail.com>
2024-10-20 18:12:07 +0200

    created logout functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -60,7 +60,11 @@ const registerUser = asyncHandler(async (req, res) => {
 // POST /api/users/logout
 // private
 const logoutUser = asyncHandler(async (req, res) => {
-  res.send('logout user');
+  res.cookie('jwt', '', {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+  res.status(200).json({ message: 'Logged out successfully' });
 });
 
 // get user profile
