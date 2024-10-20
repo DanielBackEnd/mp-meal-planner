b03981c53d544ba835f95840dd16db9e6c40d1c1
DanielBackEnd <danielgithubb@gmail.com>
2024-10-20 18:06:46 +0200

    created protect middleware, set private routes as protect by middleware

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
+import cookieParser from 'cookie-parser';
 import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import userRouter from './routes/userRoutes.js';
@@ -12,6 +13,8 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
   res.send('API is running...');
