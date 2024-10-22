a2fe8657862252ceb3e5b0945a773d3956db240c
DanielBackEnd <danielgithubb@gmail.com>
2024-10-22 23:09:26 +0200

    installed redux toolkit, config store.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,8 @@ import {
   Route,
   RouterProvider,
 } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store.js';
 import './index.css';
 import App from './App';
 import LoginScreen from './screens/LoginScreen.jsx';
@@ -23,6 +25,8 @@ const router = createBrowserRouter(
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
