e9ac9234cb08a9f31bff441e791046533c9b0b5f
DanielBackEnd <danielgithubb@gmail.com>
2024-10-22 23:13:24 +0200

    create constants.js file

---
+++ frontend/src/constants.js
@@ -0,0 +1,6 @@
+export const BASE_URL =
+  process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : '';
+export const USER_URL = '/api/users';
+export const PRODUCTS_URL = '/api/products';
+export const RECIPIES_URL = '/api/recipies';
+export const FRIDGE_URL = '/api/fridge';
