53379439b3139ed968b21756ccd59752a5b8d2d3
DanielBackEnd <danielgithubb@gmail.com>
2024-10-23 21:04:49 +0200

    added logout logic to TopBar component

--- frontend/src/components/TopBar.jsx
+++ frontend/src/components/TopBar.jsx
@@ -1,6 +1,25 @@
 import { AppBar, Toolbar, Typography, Button } from '@mui/material';
+import { useLogoutMutation } from '../slices/usersApiSlice';
+import { logout } from '../slices/authSlice';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 
 const TopBar = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const [logoutApiCall] = useLogoutMutation();
+
+  const logoutHandler = async () => {
+    try {
+      await logoutApiCall().unwrap();
+      dispatch(logout());
+      navigate('/');
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <AppBar position='static'>
       <Toolbar sx={{ justifyContent: 'space-between' }}>
@@ -11,7 +30,7 @@ const TopBar = () => {
         >
           mealPlaner
         </Typography>
-        <Button color='error' variant='contained'>
+        <Button color='error' variant='contained' onClick={logoutHandler}>
           Logout
         </Button>
       </Toolbar>
