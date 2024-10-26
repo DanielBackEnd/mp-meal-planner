35a5c06835a6060299146e4adae156c73eaefd38
DanielBackEnd <danielgithubb@gmail.com>
2024-10-26 19:19:43 +0200

    created ProductScreen

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,3 +1,4 @@
+import { useGetAllProductsQuery } from '../slices/productApiSlice';
 import TopBar from '../components/TopBar';
 import SideBar from '../components/SideBar';
 import {
@@ -10,9 +11,14 @@ import {
   TableHead,
   TableRow,
   Typography,
+  IconButton,
 } from '@mui/material';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 const ProductScreen = () => {
+  const { data: products } = useGetAllProductsQuery();
+
   return (
     <>
       <TopBar />
@@ -25,21 +31,44 @@ const ProductScreen = () => {
           >
             Products
           </Typography>
-          <TableContainer component={Paper} sx={{ marginTop: '25px' }}>
-            <Table>
-              <TableHead>
-                <TableRow>
-                  <TableCell>Product</TableCell>
-                  <TableCell>Weight</TableCell>
-                  <TableCell>Price</TableCell>
-                  <TableCell>Action</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                
-              </TableBody>
-            </Table>
-          </TableContainer>
+          {products.length > 0 ? (
+            <TableContainer component={Paper} sx={{ marginTop: '25px' }}>
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Product</TableCell>
+                    <TableCell>Weight</TableCell>
+                    <TableCell>Mark</TableCell>
+                    <TableCell>Price</TableCell>
+                    <TableCell>Action</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {products.map(product => (
+                    <TableRow
+                      key={product.id}
+                      sx={{ borderBottom: '1px solid rgba(224,224,224,1)' }}
+                    >
+                      <TableCell>{product.name}</TableCell>
+                      <TableCell>{product.weight}</TableCell>
+                      <TableCell>{product.mark}</TableCell>
+                      <TableCell>0.00</TableCell>
+                      <TableCell>
+                        <IconButton aria-label='edit'>
+                          <EditIcon />
+                        </IconButton>
+                        <IconButton aria-label='delete'>
+                          <DeleteIcon />
+                        </IconButton>
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          ) : (
+            <Typography>No products in datebase</Typography>
+          )}
         </Paper>
       </Stack>
     </>
