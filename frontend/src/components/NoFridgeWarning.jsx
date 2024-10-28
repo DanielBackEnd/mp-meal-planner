f658d193fd4c1e94c046212d16708d904a0adcfd
DanielBackEnd <danielgithubb@gmail.com>
2024-10-28 10:29:58 +0100

    created noFridgeWarning component

---
+++ frontend/src/components/NoFridgeWarning.jsx
@@ -0,0 +1,33 @@
+import { Box, Typography, Button } from '@mui/material';
+
+const NoFridgeWarning = handleCreateFridge => {
+  return (
+    <>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+        }}
+      >
+        <Typography variant='h4'>User has not fridge yet</Typography>
+        <Button
+          variant='contained'
+          color='primary'
+          size='large'
+          style={{
+            marginTop: '20px',
+            padding: '10px 20px',
+            borderRadius: '8px',
+            fontWeight: 'bold',
+            textTransform: 'none',
+          }}
+          onClick={handleCreateFridge}
+        >
+          Create New Fridge
+        </Button>
+      </Box>
+    </>
+  );
+};
+export default NoFridgeWarning;
--- frontend/src/screens/FridgeScreen.jsx
+++ frontend/src/screens/FridgeScreen.jsx
@@ -1,15 +1,37 @@
 import TopBar from '../components/TopBar';
 import SideBar from '../components/SideBar';
-import { Stack, Paper } from '@mui/material';
+import { Stack, Paper, Typography, Box } from '@mui/material';
+import NoFridgeWarning from '../components/NoFridgeWarning';
+import {
+  useGetUserFridgeQuery,
+  useCreateFridgeMutation,
+} from '../slices/fridgeApiSlice';
+import { useEffect } from 'react';
 
 const FridgeScreen = () => {
+  const { data: userFridge } = useGetUserFridgeQuery();
+  const [createFridge] = useCreateFridgeMutation();
+
+  const handleCreateFridge = async () => {
+    
+  }
+
   return (
     <>
       <TopBar />
       <Stack direction='row'>
         <SideBar />
-        <Paper sx={{ width: '100%', padding: '40px', bgcolor: 'lightgray' }}>
-          Fridge
+        <Paper
+          sx={{
+            width: '100%',
+            padding: '40px',
+            bgcolor: 'lightgray',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          {userFridge ? 'fridge' : <NoFridgeWarning />}
         </Paper>
       </Stack>
     </>
